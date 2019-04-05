c22a5395c7a8aaae0ecbf758daa367617d0fb8db
visualstudio <visualstudio@122-PC30>
2019-04-05 18:25:57 -0400

    cmmt

--- C0731847_Assignment4/Program.cs
+++ C0731847_Assignment4/Program.cs
@@ -10,7 +10,28 @@ namespace C0731847_Assignment4
     {//C0731847 gagandeep Kaur Waraich
         static void Main(string[] args)
         {
+            Program p = new Program();
+            p.Beowulf = new ArrayList();
 
         }
+        public void Run()
+        { this.ReadTextFiles(); }
+        public void ReadTextFiles()
+        {
+            //Read file using StreamReader. reads file line by line
+            using (StreamReader file = new StreamReader("c:/area51/beowulf.text"))
+            {
+                int counter = 0;
+                string ln;
+                while ((ln = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(ln);
+                    Beowulf.Add(ln);
+                }
+                file.Close();
+                Console.WriteLine($"File has {counter} lines.");
+            }
+        }
+
     }
 }
